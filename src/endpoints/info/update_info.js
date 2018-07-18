ffe3d859a7ac153dbedbae1afaf8aba88f993e29
BarJei <barjei@smartwave.ph>
2018-07-18 18:16:23 +0800

    new changes

--- src/endpoints/info/update_info.js
+++ src/endpoints/info/update_info.js
@@ -10,8 +10,10 @@ const appInfo = require('../../models/app_info'),
 
 module.exports = async function (req, res, next) {
     try {
+        console.log('damlkdawdh')
         const { _id } = req.params;
         const { privacy_policy, about, faq } = req.body;
+        console.log(req.params, req.body)
         appInfo.findByIdAndUpdate(
             _id,
             {
@@ -20,13 +22,14 @@ module.exports = async function (req, res, next) {
                 faq: faq
             }
         ).then(data => {
+            console.log(body)
             if (data) {
                 sendResponse(res, 200, CODE_SUCCESS, "Successfully Update");
             } else {
                 sendError(res, CODE_CONFLICT, MSG_CONFLICT_ERROR);
             }
         }).catch(err => {
-            console.log(err);
+            sendError(res, CODE_CONFLICT, MSG_CONFLICT_ERROR);
             throw err
         })
     } catch (err) {
