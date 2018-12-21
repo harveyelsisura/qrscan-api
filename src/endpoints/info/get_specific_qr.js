1a424442c09a9147bf3601f7caafebe19d4eecae
Kent Harvey <ken@app-venture.co>
2018-12-21 14:41:41 +0800

    QR info

--- src/endpoints/info/get_specific_qr.js
+++ src/endpoints/info/get_specific_qr.js
@@ -1,30 +1,33 @@
-const qrInfo = require('../../models/qr_info'),
-    {
-        CODE_CONFLICT,
-        MSG_CONFLICT_ERROR,
-        CODE_SUCCESS,
-        CODE_NOT_FOUND
-    } = require('../../globals/globals'),
-    { sendError, sendSuccess, sendResponse } = require('../../utils/responses_utils');
+const qrInfo = require("../../models/qr_info"),
+  {
+    CODE_CONFLICT,
+    MSG_CONFLICT_ERROR,
+    CODE_SUCCESS,
+    CODE_NOT_FOUND
+  } = require("../../globals/globals"),
+  {
+    sendError,
+    sendSuccess,
+    sendResponse
+  } = require("../../utils/responses_utils");
 
-
-
-module.exports = async function (req, res, next) {
-    try {
-        const { _id } = req.params;
-        qrInfo.findById({ _id })
-            .then(data => {
-                if (data) {
-                    sendSuccess(res, data, "Successfully Fetched")
-                } else {
-                    sendResponse(res, {}, CODE_NOT_FOUND)
-                }
-            })
-            .catch(err => {
-                throw err;
-            });
-
-    } catch (err) {
-        console.log(err)
-    }
-}
+module.exports = async function(req, res, next) {
+  try {
+    const { _id } = ObjectId(req.params);
+    console.log(_id);
+    qrInfo
+      .findById({ _id })
+      .then(data => {
+        if (data) {
+          sendSuccess(res, data, "Successfully Fetched");
+        } else {
+          sendResponse(res, {}, CODE_NOT_FOUND);
+        }
+      })
+      .catch(err => {
+        throw err;
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
